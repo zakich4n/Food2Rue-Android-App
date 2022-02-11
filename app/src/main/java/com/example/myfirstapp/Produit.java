4292a318d96d800208ddb563ac6737b0a8d83043
Paul DESCHILDRE <paul.deschildre@student.junia.com>
2022-02-11 15:01:25 +0100

    damn son, where do ya learn this ?

--- app/src/main/java/com/example/myfirstapp/Produit.java
+++ app/src/main/java/com/example/myfirstapp/Produit.java
@@ -8,3 +8,29 @@ public class Produit {
     protected String id; //the id of the product
     protected ArrayList<String> ingredients ; //An array list of an array list because the json classify the ingredients first
 }
+/* countries
+food_groups_tags
+	0:
+	1:
+images
+generic_name
+id
+ingredients
+ingredients_text
+labels
+nutrient_levels:
+	fat:
+	salt:
+	saturated-fat:
+	sugars:
+nutriments:
+	(toutes les lignes sont trop cool)
+packagings:
+product_name
+stores
+serving_quantity
+serving_size
+selected_images
+quantity
+traces
+*/
