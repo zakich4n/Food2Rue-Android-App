e00575ae241bc6af15540112b750296dbc1f534f
Paul DESCHILDRE <paul.deschildre@student.junia.com>
2022-02-22 09:48:48 +0100

    j'aime le pain

--- app/src/main/java/com/example/myfirstapp/Product.java
+++ app/src/main/java/com/example/myfirstapp/Product.java
@@ -20,9 +20,7 @@ public class Product {
     protected String categories;                    //string containing the product's categories
     protected ArrayList<String> selected_images;    //array with different pictures of the product from different countries (front, back, ingredients etc.)
 
-    public Product(                                 //simple constructor
-                                                    String _generic_name
-    ){
+    public Product(String _generic_name){           //simple constructor
         this.generic_name=_generic_name;
     }
 
@@ -61,6 +59,118 @@ public class Product {
     @Override
     public String toString() {                          //toString method
         return generic_name;
+    }
+
+    public String getGeneric_name() {
+        return generic_name;
+    }
+
+    public void setGeneric_name(String generic_name) {
+        this.generic_name = generic_name;
+    }
+
+    public ArrayList<String> getProduct_name() {
+        return product_name;
+    }
+
+    public void setProduct_name(ArrayList<String> product_name) {
+        this.product_name = product_name;
+    }
+
+    public String getBarcode() {
+        return barcode;
+    }
+
+    public void setBarcode(String barcode) {
+        this.barcode = barcode;
+    }
+
+    public String getProduct_quantity() {
+        return product_quantity;
+    }
+
+    public void setProduct_quantity(String product_quantity) {
+        this.product_quantity = product_quantity;
+    }
+
+    public ArrayList<String> getPackagings() {
+        return packagings;
+    }
+
+    public void setPackagings(ArrayList<String> packagings) {
+        this.packagings = packagings;
+    }
+
+    public String getBrands() {
+        return brands;
+    }
+
+    public void setBrands(String brands) {
+        this.brands = brands;
+    }
+
+    public String getLabels() {
+        return labels;
+    }
+
+    public void setLabels(String labels) {
+        this.labels = labels;
+    }
+
+    public String getIngredients_text() {
+        return ingredients_text;
+    }
+
+    public void setIngredients_text(String ingredients_text) {
+        this.ingredients_text = ingredients_text;
+    }
+
+    public ArrayList<String> getNutriments() {
+        return nutriments;
+    }
+
+    public void setNutriments(ArrayList<String> nutriments) {
+        this.nutriments = nutriments;
+    }
+
+    public ArrayList<String> getNutrients_levels() {
+        return nutrients_levels;
+    }
+
+    public void setNutrients_levels(ArrayList<String> nutrients_levels) {
+        this.nutrients_levels = nutrients_levels;
+    }
+
+    public String getTrace() {
+        return trace;
+    }
+
+    public void setTrace(String trace) {
+        this.trace = trace;
+    }
+
+    public ArrayList<String> getAllergens() {
+        return allergens;
+    }
+
+    public void setAllergens(ArrayList<String> allergens) {
+        this.allergens = allergens;
+    }
+
+    public String getCategories() {
+        return categories;
+    }
+
+    public void setCategories(String categories) {
+        this.categories = categories;
+    }
+
+    public ArrayList<String> getSelected_images() {
+        return selected_images;
+    }
+
+    public void setSelected_images(ArrayList<String> selected_images) {
+        this.selected_images = selected_images;
     }
 }
 
