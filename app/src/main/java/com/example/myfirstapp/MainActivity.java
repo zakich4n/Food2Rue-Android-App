322fc413e5b3d2fe5eece5e7c2f861979f27d04b
Maxime LEOPOLD <maxime.leopold@student.junia.com>
2022-02-22 10:02:49 +0100

    implemented OKHTTP library,created internet connexion and jsonobject

--- app/src/main/java/com/example/myfirstapp/MainActivity.java
+++ app/src/main/java/com/example/myfirstapp/MainActivity.java
@@ -3,8 +3,14 @@ package com.example.myfirstapp;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+
+import org.jetbrains.annotations.NotNull;
+import org.json.JSONObject;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -12,6 +18,12 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+
+import okhttp3.Call;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Callback;
+import okhttp3.Response;
 
 import javax.net.ssl.HttpsURLConnection;
 
@@ -29,7 +41,37 @@ public class MainActivity extends AppCompatActivity {
         button_connection.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                OkHttpClient client = new OkHttpClient();
+                Request request=new Request.Builder()
+                        .get()
+                        .url(URL)
+                        .build();
+                client.newCall(request).enqueue(new Callback() {
+                    private final String TAG = null;
 
+                    @Override
+                    public void onFailure(@NotNull Call call, @NotNull IOException e) {
+
+                    }
+
+                    @Override
+                    public void onResponse(@NotNull Call call, @NotNull Response response) throws IOException {
+                        if (response.isSuccessful()) {
+                            try {
+                                final String result = response.body().string();
+                                if (!TextUtils.isEmpty(result)) {
+                                    //JSONObject obj = new JSONObject(result);
+                                    //String jsonAsString = obj.toString();
+                                    TextView textview1 = (TextView) findViewById(R.id.txtDesc);
+                                    textview1.setText(result);
+                                    setContentView(textview1);
+                                }
+                            } catch (Exception e) {
+                                Log.e(TAG, "Exception = " + e);
+                            }
+                        }
+                    }
+                });
             }
         });
     }
@@ -38,50 +80,22 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         protected String doInBackground(String... strings) {
-            try {
-                URL url = new URL(strings[0]);
-                HttpsURLConnection connection = null;
-                connection = (HttpsURLConnection) url.openConnection();
-                connection.setReadTimeout(15000);
-                connection.setConnectTimeout(15000);
-                connection.setDoInput(true);
-                connection.setRequestMethod("GET");
-                connection.connect();
-                InputStream stream = connection.getInputStream();
-                BufferedReader reader = new BufferedReader(new InputStreamReader(stream));
-                StringBuilder result = new StringBuilder();
-                String line = "";
-                int counter = 0;
-                while ((line = reader.readLine()) != null) {
-                    result.append(line);
-                    counter++;
-                    publishProgress(counter / 10);
-
-                }
-                return result.toString();
-            }
-            catch (IOException e){
-                return e.getMessage();
-            }
-
-
+            return null;
         }
+
         @Override
         protected void onPreExecute(){
-            textView_progress.setVisibility(View.VISIBLE);
+
         }
 
         @Override
         protected void onPostExecute(String s){
-            Singleton.log4me(s, "onPostExecute");
-            Singleton.getInstance().parseJSON(s);
-            textView_progress.setVisibility(View.INVISIBLE);
-            recyclerView.getAdapter().notifyDataSetChanged();
+
         }
 
         @Override
         protected void onProgressUpdate(Integer... values){
-            textView_progress.setText("Downloading : " + values[0]*10+"%");
+
         }
 
 
