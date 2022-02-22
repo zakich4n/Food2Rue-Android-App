a58f0a776df460cd4f9c24e8051941a4d91dff4d
Zaki Gouizi <zaki.gouizi@student.junia.com>
2022-02-22 08:36:59 +0100

    push piur maxim

--- app/src/main/java/com/example/myfirstapp/MainActivity.java
+++ app/src/main/java/com/example/myfirstapp/MainActivity.java
@@ -1,26 +1,31 @@
 package com.example.myfirstapp;
 
-import android.content.Context;
-import android.net.ConnectivityManager;
-import android.net.Network;
-import android.net.NetworkCapabilities;
-import android.net.NetworkInfo;
-import android.os.Build;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-import java.net.InetAddress;
-import java.net.UnknownHostException;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import javax.net.ssl.HttpsURLConnection;
 
 public class MainActivity extends AppCompatActivity {
+    //public static String data_received = "";
+    public static String URL = "https://world.openfoodfacts.org/api/v2/product/04963406";
+    public String data = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Button button_connection = (Button) findViewById(R.id.button_connected);
+
         button_connection.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -29,18 +34,57 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-    public boolean isNetworkAvailable(Context context) {
-        ConnectivityManager connectivityManager = ((ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE));
-        return connectivityManager.getActiveNetworkInfo() != null && connectivityManager.getActiveNetworkInfo().isConnected();
+    public class DownloadAsyncTask extends AsyncTask<String,Integer,String> {
+
+        @Override
+        protected String doInBackground(String... strings) {
+            try {
+                URL url = new URL(strings[0]);
+                HttpsURLConnection connection = null;
+                connection = (HttpsURLConnection) url.openConnection();
+                connection.setReadTimeout(15000);
+                connection.setConnectTimeout(15000);
+                connection.setDoInput(true);
+                connection.setRequestMethod("GET");
+                connection.connect();
+                InputStream stream = connection.getInputStream();
+                BufferedReader reader = new BufferedReader(new InputStreamReader(stream));
+                StringBuilder result = new StringBuilder();
+                String line = "";
+                int counter = 0;
+                while ((line = reader.readLine()) != null) {
+                    result.append(line);
+                    counter++;
+                    publishProgress(counter / 10);
+
+                }
+                return result.toString();
+            }
+            catch (IOException e){
+                return e.getMessage();
+            }
+
+
+        }
+        @Override
+        protected void onPreExecute(){
+            textView_progress.setVisibility(View.VISIBLE);
+        }
+
+        @Override
+        protected void onPostExecute(String s){
+            Singleton.log4me(s, "onPostExecute");
+            Singleton.getInstance().parseJSON(s);
+            textView_progress.setVisibility(View.INVISIBLE);
+            recyclerView.getAdapter().notifyDataSetChanged();
+        }
+
+        @Override
+        protected void onProgressUpdate(Integer... values){
+            textView_progress.setText("Downloading : " + values[0]*10+"%");
+        }
+
+
     }
 
-    public boolean isInternetAvailable() {
-        try {
-            InetAddress address = InetAddress.getByName("www.google.com");
-            return !address.equals("");
-        } catch (UnknownHostException e) {
-            // Log error
-        }
-        return false;
-    }
 }
