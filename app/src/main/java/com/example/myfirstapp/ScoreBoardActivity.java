ade45beea06ee30869ca88b3950dbe389d659bb1
JTBlanqui <julien-tien.blanqui@student.junia.com>
2022-02-27 00:11:54 +0100

    hide the default action bar

--- app/src/main/java/com/example/myfirstapp/ScoreBoardActivity.java
+++ app/src/main/java/com/example/myfirstapp/ScoreBoardActivity.java
@@ -14,6 +14,8 @@ public class ScoreBoardActivity extends AppCompatActivity {
     protected void onCreate( Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        getSupportActionBar().hide();
+
         setContentView(R.layout.scoreboard);
 
 
